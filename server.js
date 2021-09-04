624790ba53532506dd013a70b0b2201e5af2e949
hilalsidhic <hilalsidhic21@gmail.com>
2021-09-04 18:38:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const PORT= 3000 || process.env.PORT;
+const server =http.createServer(app);
+const io = socketio(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket)=>{
+
+    socket.emit('message',"welcome to chat");
+
+    socket.broadcast.emit('message',"A user has joined the chat room")
+
+    socket.on('disconnect', ()=>{
+        io.emit('message','user has disconnected from the chat')
+    })
+})
+
+server.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`);
+});
